05518bf7b7a5c48a66b1cd386a16d1a4a8d12070
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2026-06-23 12:39:18 +0530

    feat: migrate to vite

--- src/index.jsx
+++ src/index.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { LocationProvider } from './context/LocationContext';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <HashRouter>
+  <BrowserRouter>
     <LocationProvider>
       <App />
     </LocationProvider>
-  </HashRouter>
+  </BrowserRouter>
 );
 
 
